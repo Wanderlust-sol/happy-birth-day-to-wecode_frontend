8c1d77a48f6dfc783e8717e0f478afa85c4d320e
unknown <hyungmin.b.shim@gmail.com>
2020-04-10 01:14:03 +0900

    main root url wip

--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import Main from "./pages/Main";
 import ArtList from "components/ArtList";
 import ModalVote from "./components/Modal";
 import Contributor from "./pages/Contributor";
+import Header from "components/Header";
 import Footer from "components/Footer";
 import Result from "pages/Result";
 
@@ -13,12 +14,15 @@ class Routes extends React.Component {
   render() {
     return (
       <Router>
+        <Header />
         <Switch>
-          <Route exact path="/modal" component={ModalVote} />
-          <Route exact path="/contributor" component={Contributor} />
           <Route exact path="/" component={Main} />
+          <Route exact path="/poem3" component={Main} />
+          <Route exact path="/pic" component={Main} />
+          <Route exact path="/poem" component={Main} />
           <Route exact path="/art" component={ArtList} />
           <Route exact path="/modal" component={ModalVote} />
+          <Route exact path="/contributor" component={Contributor} />
           <Route exact path="/result" component={Result} />
         </Switch>
         <Footer />
