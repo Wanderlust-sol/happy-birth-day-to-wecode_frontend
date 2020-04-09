9187bf3fbfccbb3d0048c743e09a40370dab82bd
unknown <hyungmin.b.shim@gmail.com>
2020-04-09 23:25:29 +0900

    wip

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import styled, { css } from "styled-components";
+import backimg from "../img/pattern-bg.svg";
+import anivimg from "../img/title@2x.png";
 
 class Main extends Component {
   render() {
     return (
-      <>
-        <MainWrap>
-          <MainTitle>a</MainTitle>
-        </MainWrap>
-      </>
+      <MainWrap>
+        <MainTitle>> wecode</MainTitle>
+        <MainBody>
+          <MainAniv src={anivimg}></MainAniv>
+          <MainTabWrap>
+            <MainTabThreePoem></MainTabThreePoem>
+            <MainTabPicture></MainTabPicture>
+            <MainTabPoem></MainTabPoem>
+          </MainTabWrap>
+        </MainBody>
+      </MainWrap>
     );
   }
 }
@@ -16,10 +24,47 @@ class Main extends Component {
 export default Main;
 
 const MainWrap = styled.div`
-  width: 1440px;
+  // width: 1440px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
 `;
 
 const MainTitle = styled.div`
+  width: 100%;
+  height: 64px;
+  background-color: #000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin: 0 auto;
-  width: 1440px;
+  color: white;
+  font-weight: 700;
+  font-size: 23px;
 `;
+
+const MainBody = styled.div`
+  width: 100%;
+  height: 2000px;
+  background-image: url(${backimg});
+`;
+
+const MainAniv = styled.img`
+  position: absolute;
+  width: 320px;
+  // height: 210px;
+  left: 50%;
+  top: 99px;
+  transform: translateX(-50%);
+`;
+
+const MainTabWrap = styled.div`
+  width: 95%;
+  margin: 0 auto;
+`;
+
+const MainTabThreePoem = styled.div``;
+
+const MainTabPicture = styled.div``;
+
+const MainTabPoem = styled.div``;
